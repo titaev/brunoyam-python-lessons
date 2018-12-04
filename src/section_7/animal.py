9cf2067f4dcd2e8b2d68458490ad0316283d9a8c
Nora <oleg.yurchik@quest.com>
2018-12-05 00:14:46 +0300

    Add src code for section 7

--- src/section_7/animal.py
+++ src/section_7/animal.py
@@ -1,23 +1,33 @@
-climats = ["cold", "warm", "hot"]
-
 class Animal:
     def __init__(self, name):
         self.name = name
         self.square = None
         self.climat = None
 
-# Создать класс белого медведя
-# Площадь = 40
-# Климат = "cold"
 
-# Создать класс пингвина
-# Площадь = 7
-# Климат = "cold"
+class WhiteBear(Animal):
+    def __init__(self, name):
+        self.name = name
+        self.square = 40
+        self.climat = "cold"
 
-# Создать класс лошади
-# Площадь = 200
-# Климат = "warm"
 
-# Создать класс Гепарда
-# Площадь = 30
-# Климат = "hot"
+class Pinguin(Animal):
+    def __init__(self, name):
+        self.name = name
+        self.square = 7
+        self.climat = "cold"
+
+
+class Horse(Animal):
+    def __init__(self, name):
+        self.name = name
+        self.square = 200
+        self.climat = "warm"
+
+
+class Gepard(Animal):
+    def __init__(self, name):
+        self.name = name
+        self.square = 30
+        self.climat = "hot"
