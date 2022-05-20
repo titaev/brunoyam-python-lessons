75ed57020d763009b02f9bc44d5e14b053842878
ititaev <i.titaev@ringme.ru>
2022-05-20 19:00:56 +0300

    add graphs

--- graphs.py
+++ graphs.py
@@ -10,6 +10,22 @@ def search_in_depth():
     pass
 
 
-def search_in_width():
-    pass
+visited = []  # List to keep track of visited nodes.
+queue = []   # Initialize a queue
 
+
+def search_in_width(visited, graph, node):
+    global queue
+    visited.append(node)
+    queue.append(node)
+    while queue:
+        s = queue.pop(0)
+        print(s, end=" ")
+        for neighbour in graph[s]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+
+
+# Driver Code
+search_in_width(visited, graph, 'A')
