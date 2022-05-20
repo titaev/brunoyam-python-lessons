c099748a86e4a3640aa2c73fa5f7b860dd410c63
ititaev <i.titaev@ringme.ru>
2022-05-20 18:54:07 +0300

    add graphs

---
+++ graphs.py
@@ -0,0 +1,15 @@
+graph = {'A': ['B', 'C'],
+         'B': ['A', 'D', 'E'],
+         'C': ['A', 'F'],
+         'D': ['B'],
+         'E': ['B', 'F'],
+         'F': ['C', 'E']}
+
+
+def search_in_depth():
+    pass
+
+
+def search_in_width():
+    pass
+
